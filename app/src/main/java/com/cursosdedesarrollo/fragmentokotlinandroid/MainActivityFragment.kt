0fe99f5cecd7a601a8ebe0cb0ad1263ca3d185e4
David Vaquero <pepesan@gmail.com>
2018-07-17 19:31:56 +0200

    Initial commit

---
+++ app/src/main/java/com/cursosdedesarrollo/fragmentokotlinandroid/MainActivityFragment.kt
@@ -0,0 +1,28 @@
+package com.cursosdedesarrollo.fragmentokotlinandroid
+
+import android.support.v4.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_main.*
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+class MainActivityFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        button.setOnClickListener({
+            texto.text="Texto Cambiado"
+        })
+    }
+
+
+}
